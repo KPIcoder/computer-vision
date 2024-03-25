4776bb17797758b3a0e766a328e3922bc6ba7031
kpicoder <vladshcheglov41@gmail.com>
2024-03-25 10:33:22 +0000

    lab3

--- task2.py
+++ task2.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt
 
 image = cv2.imread('cv_lab_image.jpeg')
 
@@ -10,8 +11,19 @@ edges = cv2.Canny(blurred, 50, 150)
 
 contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+countrered = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
 
-cv2.imshow('Contours', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def show_image_with_matplotlib(img, title):
+    # Convert BGR image to RGB
+    img_RGB = img[:, :, ::-1]
+    plt.imshow(img_RGB)
+    plt.title(title)
+    plt.axis('off')
+
+plt.figure(figsize=(20, 10))
+
+plt.subplot(151), show_image_with_matplotlib(cv2.imread('cv_lab_image.jpeg'), 'Original')
+plt.subplot(152), show_image_with_matplotlib(countrered, 'Contours Applied')
+
+plt.show()
